9d29dbc28a9fbb908f8f4cfdeb49b06ebb1dc5bf
rADikal8e7 <gautham.csus@gmail.com>
2020-03-30 14:00:05 -0700

    Initial commit for EPI problems

---
+++ EPI python/4_0_count_bits_set_to_1.py
@@ -0,0 +1,21 @@
+'''
+Count the number of bits that are set to 1 in a positive integer.
+
+Algo:
+1.init a var to zero to hold the number of bits
+2.while x is not zero go into the loop and check the bitwise AND operation of x and 1
+3.Add the result of the AND operation to the var that is holding the number of bits.
+4.right shift the number by 1 bit using bitwise right shift operator and continue the while loop
+5.Return the number of bits.
+'''
+
+def count_bits(x):
+    num_bits=0
+    while x:
+        num_bits += x & 1
+        x >>= 1
+    return num_bits
+
+if __name__ == "__main__":
+    i=count_bits(10)
+    print(i)
